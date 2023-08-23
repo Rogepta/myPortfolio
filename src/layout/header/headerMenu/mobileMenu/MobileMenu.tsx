a053e4d0b2bc5e9da0056451004729d7bf8c7b08
Daniil Dergunov <100157798+Rogepta@users.noreply.github.com>
2023-08-23 17:07:26 +0300

    Add hook to header

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,16 +1,24 @@
 import { Social } from "../../../../components/social/Social";
 import { Menu } from "../menu/Menu";
 import { S } from "../HeaderMenu_Styles";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 export const MobileMenu: FC = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const onBtnBurgerClick = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
   return (
     <S.MobileMenu>
-      <S.BurgerButton isOpen={false}>
+      <S.BurgerButton isOpen={menuIsOpen} onClick={onBtnBurgerClick}>
         <span></span>
       </S.BurgerButton>
 
-      <S.MobileMenuPopUp isOpen={false}>
+      <S.MobileMenuPopUp
+        isOpen={menuIsOpen}
+        onClick={() => setMenuIsOpen(false)}
+      >
         <Menu />
         <Social />
       </S.MobileMenuPopUp>
